0acde20b0af62456cce916b4d982f6faf0195889
suheyl-yazici <93797206+suheyl-yazici@users.noreply.github.com>
2022-04-23 03:27:22 +0300

    çalışmaya devam edildi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,92 +17,89 @@ import cwLogo from "../assets/cw.jpeg";
 
 
 const Navbar = () => {
-  const navigate = useNavigate();
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-
+  
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
-
+  
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+  const navigate = useNavigate();
+
+
+  const currentUser = true ;
 
   return (
     <AppBar position="static">
       <Container maxWidth="xxl">
-        <Toolbar style={{ justifyContent: "space-between" }}>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { md: "flex" } }}
-          >
-            <Button
-              sx={{ my: 2, display: "block" }}
+          <Toolbar style={{ justifyContent: "space-between" }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "flex" } }}
+            >
+              <Button
+                sx={{ my: 2, display: "block"}}
+                onClick={() => navigate("/")}
+              >
+                <img src={cwLogo} alt="cwlogo" height={50} width={60}/>
+              </Button>
+            </Typography>
+
+            <Box
+              sx={{ flexGrow: 1, display: { xs: "none", md: "flex",justifyContent: "center", alignItems: "center" } }}
               onClick={() => navigate("/")}
             >
-              <img src={cwLogo} alt="cwlogo" height={25} width={30}/>
-            </Button>
-          </Typography>
+              <Button sx={{ my: 2, color: "white", display: "block" ,fontSize:"1.5rem"}}>
+              ── SUHEYL BLOG ──
+              </Button>
+            </Box>
 
-          <Box
-            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
-            style={{ justifyContent: "center", alignItems: "center" }}
-            onClick={() => navigate("/")}
-          >
-            <div>çizgi</div>
-            <Button sx={{ my: 2, color: "white", display: "block" }}>
-              -- SüheyL Blok--
-            </Button>
-            <div>çizgi</div>
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton
-                onClick={handleOpenUserMenu}
-                sx={{ p: 0 }}
-                size="large"
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton
+                  onClick={handleOpenUserMenu}
+                  sx={{ p: 0 }}
+                >
+                  <AccountCircle style={{ color: "white", height:40,width:40}} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
               >
-                <AccountCircle style={{ color: "white", height:45,width:45}} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem>
-                <Typography
-                  textAlign="center"
-                  onClick={() => navigate("/login")}
-                >
-                  Login
-                </Typography>
-              </MenuItem>
-              <MenuItem>
-                <Typography
-                  textAlign="center"
-                  onClick={() => navigate("/register")}
-                >
-                  Register
-                </Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
+                {currentUser ? (<>
+                  <MenuItem onClick={() => {navigate("/login");setAnchorElUser(null)}}>
+                  <Typography textAlign="center">Login</Typography>
+                </MenuItem>
+                
+                <MenuItem>
+                  <Typography
+                    textAlign="center"
+                    onClick={() => {navigate("/register");setAnchorElUser(null)}}
+                  >
+                    Register
+                  </Typography>
+                </MenuItem>
+                </>) : ("merhaba")}
+              </Menu>
+            </Box>
+          </Toolbar>
       </Container>
     </AppBar>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,63 +5,95 @@ import CardContent from "@mui/material/CardContent";
 import { Box, Stack } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
 import "../App.css";
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 import googleImg from "../assets/google.png";
-
 
 const Login = () => {
   return (
-    <div style={{
-      backgroundImage: `url("https://picsum.photos/800/800")`,
-      height:'100vh',
-      marginTop:'-70px',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat'}}
-      >
-    <Container maxWidth="sm" className="container"> 
-    <Card sx={{ minWidth: 275 }} style={{ width: 500, height: 650 ,marginTop:70,display:"flex", justifyContent:"center"}}>
-      <div style={{display:"flex", flexDirection:"column"}}>
-        <div className="card-header">
-            <Avatar
-              alt="Remy Sharp"
-              src={BlokImg}
-              sx={{ width: 200, height: 200 }}
-            />
-        </div>
-        <div className="card-header-text">
-          <Typography sx={{ fontSize: 30 }} color="text.secondary" gutterBottom >
-            --LOGIN--
-          </Typography>
-      </div>
-        <CardContent>
-          <Box style={{ backgroundColor: "white", padding: "20px" }}>
-            <form>
-              <Stack spacing={2} direction="column">
-              <TextField id="email" label="Email" variant="outlined" required
-              helperText="Email is required"/>
-              <TextField
-              required
-                    id="outlined-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    helperText="Password is required"
-        />
-
-              </Stack>
-              <Stack spacing={2} direction="column" style={{marginTop:"20px"}}>
-                <Button variant="contained">LOGIN</Button>
-                <Button variant="outlined">With<img src={googleImg} alt="google" height={30} width={45}/></Button>
-              </Stack>
-            </form>
-          </Box>
-        </CardContent>
-        </div>
-    </Card>
-    </Container>
+    <div
+      style={{
+        backgroundImage: `url("https://picsum.photos/800/800")`,
+        height: "90vh",
+        marginTop: "-570px",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        boxSizing: "border-box",
+      }}
+    >
+      <Container maxWidth="sm">
+        <Card
+          sx={{
+            minWidth: 275,
+            width: 500,
+            height: 650,
+            marginTop: 70,
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "red",
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div className="card-header">
+              <Avatar
+                alt="blog"
+                src={BlokImg}
+                sx={{ width: 200, height: 200 }}
+              />
+            </div>
+            <div className="card-header-text">
+              <Typography
+                sx={{ fontSize: 30 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                ── LOGIN ──
+              </Typography>
+            </div>
+            <CardContent>
+              <Box style={{ backgroundColor: "white", padding: "20px" }}>
+                <form>
+                  <Stack spacing={2} direction="column">
+                    <TextField
+                      id="email"
+                      label="Email"
+                      variant="outlined"
+                      required
+                      helperText="Email is required"
+                    />
+                    <TextField
+                      required
+                      id="outlined-password-input"
+                      label="Password"
+                      type="password"
+                      autoComplete="current-password"
+                      helperText="Password is required"
+                    />
+                  </Stack>
+                  <Stack
+                    spacing={2}
+                    direction="column"
+                    style={{ marginTop: "20px" }}
+                  >
+                    <Button variant="contained">LOGIN</Button>
+                    <Button variant="outlined">
+                      With
+                      <img
+                        src={googleImg}
+                        alt="google"
+                        height={25}
+                        width={40}
+                      />
+                    </Button>
+                  </Stack>
+                </form>
+              </Box>
+            </CardContent>
+          </div>
+        </Card>
+      </Container>
     </div>
   );
 };
